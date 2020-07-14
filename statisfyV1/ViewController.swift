13d4ff0ce8853bbea8394f81a855ed0965c26417
Stephanie Su <stephanie.fenhua@gmail.com>
2020-07-14 13:32:10 -0700

    Iteration 0

--- statisfyV1/ViewController.swift
+++ statisfyV1/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        startButton.layer.cornerRadius = 5
         // Do any additional setup after loading the view.
     }
 
